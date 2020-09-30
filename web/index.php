a9c4cb5f20a409b5dcd234e0462d3cf23e7a4f62
Dayan Monteiro <dmsmamb@gmail.com>
2020-09-30 13:48:06 -0300

    teste

---
+++ web/index.php
@@ -0,0 +1,2 @@
+<?php $mensagem= 'Hello Word!'; /*o $ guarda a variável*/
+    echo $mensagem; /* A função echo é a instrução que envia para a saída qualquer informação, podendo conter texto, números ou variáveis. */
