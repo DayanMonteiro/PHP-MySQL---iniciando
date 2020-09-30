e12804d7f4e940642fefdc2c2468354b92d1508d
Dayan Monteiro <dmsmamb@gmail.com>
2020-09-30 14:16:48 -0300

    operações aritméticas

--- web/index.php
+++ web/index.php
@@ -1,7 +1,28 @@
 <?php
-
+/*operações aritméticas*/
 $A = 2;
 $B = 3;
 
+/* Soma
 $soma = $A + $B;
-print $soma;
+print $soma; */
+
+/* subtração*/
+$subtraction = $A - $B;
+print $subtraction;
+
+/*miltiplicação
+$multiplication = $A * $B;
+print $multiplication;*/
+
+/*divisão
+$division = $A / $B;
+print $division;*/
+
+/*exponenciação
+$exponentiation = $A ** $B;
+print $exponentiation;*/
+
+/*módulo (resto da divisão)
+$module = $A % $B;
+print $module;*/
