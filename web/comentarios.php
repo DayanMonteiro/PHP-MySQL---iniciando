9e1d9d737de49459e6ef2c9109ab1037e2444031
Dayan Monteiro <dmsmamb@gmail.com>
2020-10-01 20:17:54 -0300

    comentários

---
+++ web/comentarios.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+    <meta charset="utf-8"/>
+     <title>Curso PHP</title>
+    </head>
+
+    <body>
+        <!--comentário html - os comentários html aparecem na inspeção-->
+    <?php 
+    // comentário de uma linha
+    echo'Comando echo<br/>';
+    # comentário de uma linha
+    print 'Comando print<hr/>';
+
+    /*
+    comentário de multiplas linhas.
+    todos os tipos de comentários php 
+    não aparecem nas inspeções dos usuários
+    */
+    ?>
+
+
+
+    </body>
+
+</html>
